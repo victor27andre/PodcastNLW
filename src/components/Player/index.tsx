7fbe24d1a9c407e7237496b01d19eec53bff26e2
Victor André <victor.costa@teclat.com.br>
2021-04-20 17:25:16 -0300

    Aula 2 - pela metade

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,43 @@
+import styles from './styles.module.scss';
+
+export function Player() {
+    return (
+        <div className={styles.playerContainer}>
+            <header>
+                <img src="/playing.svg" alt='Tocando agora' />
+                <strong>Tocando agora</strong>
+            </header>
+
+            <div className={styles.emptyPlayer}>
+                <strong>Selecione um podcast para ouvir</strong>
+            </div>
+
+            <footer>
+                <div className={styles.progress}>
+                    <span>00:00</span>
+                    <div className={styles.emptySlider} />
+                    <span>00:00</span>
+                </div>
+
+                <div className={styles.buttons}>
+                    <button type="button">
+                        <img src="/shuffle.svg" alt="Embaralhar" />
+                    </button>
+                    <button type="button">
+                        <img src="/play-previous.svg" alt="Tocar anterios" />
+                    </button>
+                    <button type="button">
+                        <img src="/play.svg" alt="Tocar" />
+                    </button>
+                    <button type="button">
+                        <img src="/play-next.svg" alt="Tocar proxima" />
+                    </button>
+                    <button type="button">
+                        <img src="/repeat.svg" alt="Repetir" />
+                    </button>
+                </div>
+            </footer>
+
+        </div>
+    );
+}
